8144ca97585b04943cbb0e18d98a6089f1d618d2
Ben Bowers <benjambowers@gmail.com>
2021-09-21 13:49:06 -0400

    Switched from nodemailer to sendgrid

--- client/pages/api/mail.ts
+++ client/pages/api/mail.ts
@@ -1,33 +1,21 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import nodemailer from 'nodemailer';
+import Mail from '@sendgrid/mail';
 
 export default (req: VercelRequest, res: VercelResponse) => {
-	console.log('req: ', req);
-	console.log('res: ', res);
+	Mail.setApiKey(process.env.SENDGRID_API_KEY as string);
 
-	const email = process.env.EMAIL_ADDR;
-	const pass = process.env.EMAIL_PASS;
-
-	const transporter = nodemailer.createTransport({
-		service: 'gmail',
-		auth: {
-			user: email,
-			pass: pass,
-		},
-	});
-
-	const message = {
-		from: 'me@benbowers.net',
-		to: 'me@benbowers.net',
-		subject: 'From: ' + req.body.email,
+	const msg = {
+		to: 'me@benbowers.net', // Change to yo
+		from: 'mail-bot@benbowers.net',
+		subject: 'Message from ' + req.body.email,
 		text: req.body.message,
 	};
 
-	transporter.sendMail(message, (err, info) => {
-		if (err) {
-			res.status(400).json(err);
-		} else {
-			res.status(200).json(info);
-		}
-	});
+	Mail.send(msg)
+		.then(() => {
+			res.status(200).send(req.body);
+		})
+		.catch((error) => {
+			res.status(400).json(error);
+		});
 };
