8269208fafb9114bb5df91c8fa00930b818d8f6c
Benjamin R Bowers <benjambowers@gmail.com>
2022-04-10 02:29:25 +0000

    Close settings when user clicks away

--- client/components/Settings.tsx
+++ client/components/Settings.tsx
@@ -1,5 +1,5 @@
 import { CloseIcon } from '@chakra-ui/icons';
-import { IconButton } from '@chakra-ui/react';
+import { Box, Flex, IconButton } from '@chakra-ui/react';
 import Theme from '@chakra-ui/theme';
 import React, { useContext, useState } from 'react';
 import { ThemeContext } from '../themes/theme';
@@ -36,37 +36,45 @@ export const Settings: React.FC<SettingsProps> = ({
 					setActive(!active);
 				}}
 			/>
-			<SettingsMenu
-				zIndex={2}
-				pos="fixed"
-				transition={`left ${animationDuration}s`}
-				left={active ? '0px' : '-305px'}
-				top={0}
-			>
-				<IconButton
-					visibility={['visible', 'hidden']}
-					w={8}
-					h={8}
-					m={3}
-					zIndex={3}
-					pos="absolute"
-					top="0px"
-					right="0px"
-					aria-label="close settings"
-					className="closeSettings"
-					icon={<CloseIcon />}
-					color="gray.500"
-					bgColor="white"
-					_focus={{
-						boxShadow:
-							'0px 0px 0px 3px ' +
-							Theme.colors[colorTheme as keyof typeof Theme.colors]['300'],
-					}}
+			<Flex w="full" h="full" flexDir="row" pos="fixed" top={0} zIndex={2}>
+				<SettingsMenu
+					pos="relative"
+					transition={`left ${animationDuration}s`}
+					left={active ? '0px' : '-305px'}
+				>
+					<IconButton
+						visibility={['visible', 'hidden']}
+						w={8}
+						h={8}
+						m={3}
+						zIndex={3}
+						pos="absolute"
+						top="0px"
+						right="0px"
+						aria-label="close settings"
+						className="closeSettings"
+						icon={<CloseIcon />}
+						color="gray.500"
+						bgColor="white"
+						_focus={{
+							boxShadow:
+								'0px 0px 0px 3px ' +
+								Theme.colors[colorTheme as keyof typeof Theme.colors]['300'],
+						}}
+						onClick={() => {
+							setActive(false);
+						}}
+					/>
+				</SettingsMenu>
+				<Box
+					h="full"
+					flexGrow={1}
+					pointerEvents={active ? 'auto' : 'none'}
 					onClick={() => {
 						setActive(false);
 					}}
 				/>
-			</SettingsMenu>
+			</Flex>
 		</>
 	);
 };
